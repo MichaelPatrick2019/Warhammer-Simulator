cecfcaeb367fc1e95cbaaef68af087632898841c
Michael Patrick <memep3@gmail.com>
2021-02-01 09:43:15 -0800

    adding potential main program

---
+++ main.cpp
@@ -0,0 +1,63 @@
+/** @main.cpp */
+
+/** Program that runs a loop that takes in an army of warhammer characters,
+initializes their values, and provides combat with simulated dice rolls. 
+
+Michael Patrick
+2/1/21 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   cout << "Welcome to Warhammer 40k! Please enter you army details below...";
+   cout << endl;
+
+   /**
+   bool notDone = true;
+
+   Army playerArmy;
+   Army enemyArmy;
+
+   while (notDone) {
+      Character character;
+      string name;
+
+      cout << "Please enter your character's name...";
+      cin >> name;
+      character.setName(name);
+
+      cout << "Please enter your character's stats in the following format: ";
+      cout << "[M] [WS] [BS] [S] [T] [W] [A] [Ld] [Sv]";
+      cout << endl;
+
+      string stats;
+      cin >> stats;
+      character.setStats(stats);
+
+      cout << "Please enter your character's ranged weapons. If your";
+      cout << "character doesn't have any ranged attacks," << endl;
+      cout << "please enter 'none' instead.";
+      cout << "[Range] [Type] [number of attacks] [S] [AP] [Abilities]";
+
+      string ranged;
+      cin >> ranged;
+      character.setRanged(ranged);
+
+      cout << "Please enter your character's melee weapons. If your";
+      cout << "character doesn't have any ranged attacks," << endl;
+      cout << "please enter 'none' instead";
+      cout << "[S] [AP] [D] [Abilities]";
+
+      string melee;
+      cin >> ranged;
+      character.setMelee(melee);
+
+      cout << "Do you have another character to add? Please respond" <<
+         "'Y' or 'N'";
+   }
+   */
+
+}
