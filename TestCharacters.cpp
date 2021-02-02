9f720955873aa3d7a0c50268bed931f583f7b8b4
Michael Patrick <memep3@gmail.com>
2021-02-02 13:51:23 -0800

    Slightly modified testing function

--- TestCharacters.cpp
+++ TestCharacters.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include "Character.h";
+#include <assert.h>
 
 
 //Test Character.cpp
@@ -11,12 +12,22 @@ using namespace std;
 int main()
 {
    Character character;
+
    string test = "  A  b c               d   ";
    vector<string>* strVector = character.split(" ", test);
 
+   string result = "";
+
    for (int i = 0; i < strVector->size(); i++) {
-      cout << strVector->at(i) << " ";
+      result += strVector->at(i);
    }
 
+   assert(result == "Abcd");
+
    delete strVector;
+
+   character.setName("John Krasinski");
+   character.setStats("11 4 5 4 4 4 6 10 3 5");
+
+   cout << character;
 }
