ff46696e52e04cfb97bb156f7bddcbce60310e70
Michael Patrick <memep3@gmail.com>
2021-02-08 22:51:54 -0800

    Quick brainstorm on Army classes. Needs documentation.

--- Army.h
+++ Army.h
@@ -2,10 +2,11 @@
 
 /** @Army.h */
 /** Army class that stores multiple Character objects
-in a binary search tree. Intended to provide quick
+in a AVL tree. Intended to provide quick
 access to characters sorted by name. */
 
 #include "Character.h"
+#include <fstream>
 
 class Army
 {
@@ -24,6 +25,20 @@ private:
 
 
 
+public:
+
+   Army();
+
+   //Need a way to read in armies from a file...
+   Army(ifstream& inFile);
+   ~Army();
+
+   bool add(Character* newChar);
+   Character* retrieve(string name); //Get Character ptr by name
+   void rotate(Node* node); //AVL tree...
+   int size(); //Return size
+
+   friend ostream& operator<<(ostream& os, const Army& army);
 
 };
 
